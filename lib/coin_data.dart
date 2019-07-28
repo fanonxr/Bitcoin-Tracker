9a5580a1a03892c5ef72f6e88e39cdd1f21d76b5
FanonX Rogers <fanonxdevelops@gmail.com>
2019-07-28 15:46:58 -0400

    Displaying the mutiple crypto currencies conversions for mutiple currencies.

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,35 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const bitcoinAverageUrl = 'https://apiv2.bitcoinaverage.com/indices/global/ticker';
+
+class CoinData {
+  // get the data from the api
+  Future getCoinData(String selectedCurrency) async {
+    // loop through the crypto list and make a request -> store the date in a map
+    Map<String, String> cryptoPrices= {};
+
+    for (String crypto in cryptoList) {
+      // update the request url in order to get correct data
+      String requestURL = '$bitcoinAverageUrl/$crypto$selectedCurrency';
+      http.Response response = await http.get(requestURL);
+
+      // check if valid request
+      if (response.statusCode == 200) {
+        // successful
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['last'];
+
+        // store the price with its type of currency
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        // unsuccessful
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
+
+    }
+    // returns the map of the crypto price
+    return cryptoPrices;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -10,7 +10,7 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   // property of selected currency
-  String selectedCurrency = 'USD';
+  String selectedCurrency = 'AUD';
 
   DropdownButton<String> androidDropDown() {
 
@@ -34,6 +34,8 @@ class _PriceScreenState extends State<PriceScreen> {
         setState(
           () {
             selectedCurrency = value;
+            // get data when the dropdown changes
+            getData();
           },
         );
       },
@@ -49,55 +51,117 @@ class _PriceScreenState extends State<PriceScreen> {
     }
 
     return CupertinoPicker(
-      backgroundColor: Colors.lightBlue,
+      backgroundColor: Colors.green,
       itemExtent: 32.0,
       onSelectedItemChanged: (selectedIndex) {
-        print(selectedIndex);
+        setState(() {
+          // save the selectedIndex -> curr
+          selectedCurrency = currenciesList[selectedIndex];
+          // call get data to update with selected currency
+          getData();
+        });
       },
       children: pickerItems,
     );
   }
 
+  // store the prices in a map
+  Map<String, String> coinValues = {};
+  bool isWaiting = false;
+
+  // method to get data from api and make the request
+  String bitcoinValueInUSD = '?';
+
+  void getData() async {
+    isWaiting = true;
+    try {
+      var data = await CoinData().getCoinData(selectedCurrency); // this is a map
+      isWaiting = false;
+      // update the state
+      setState(() {
+       coinValues = data;
+      });
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
+
+  // as soon as the app loads, update the state with the data fetch from the API
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('🤑 Coin Ticker'),
+        backgroundColor: Colors.green,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          CryptoCard(
+            cryptoCurrency: 'BTC',
+            value: isWaiting ? '?' : coinValues['BTC'],
+            selectedCurrency: selectedCurrency,
+          ),
+          CryptoCard(
+            cryptoCurrency: 'ETH',
+            value: isWaiting ? '?' : coinValues['ETH'],
+            selectedCurrency: selectedCurrency,
+          ),
+          CryptoCard(
+            cryptoCurrency: 'LTC',
+            value: isWaiting ? '?' : coinValues['LTC'],
+            selectedCurrency: selectedCurrency,
           ),
           Container(
               height: 150.0,
               alignment: Alignment.center,
               padding: EdgeInsets.only(bottom: 30.0),
-              color: Colors.lightBlue,
+              color: Colors.green,
               child: Platform.isIOS ? iosPicker() : androidDropDown(),
           ),
         ],
+      ),
+    );
+  }
+}
+
+// make a crypto card that will hold the data for mutiple currencys
+class CryptoCard extends StatelessWidget {
+
+  CryptoCard({this.value, this.selectedCurrency, this.cryptoCurrency});
+
+  final String value;
+  final String selectedCurrency;
+  final String cryptoCurrency;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.green,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $cryptoCurrency = $value $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     );
   }
